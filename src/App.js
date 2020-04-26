f056a2f0e79b5c0215feeab776b3e90b115fb7f4
ruturajkanake <ruturajkanake1999@gmail.com>
2020-04-26 15:58:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider, connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ADD = 'ADD'
+const addMessage = (message)=>{
+  return {
+    type: ADD,
+    message: message
+  }
 }
 
-export default App;
+class Presentational extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      input: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(e){
+
+    this.setState({
+      input: e.target.value
+    })
+  }
+  submitMessage(e){
+    e.preventDefault();
+
+    this.props.submitNewMessage(this.state.input);
+    this.setState({
+      input: '',
+    })
+    
+  }
+  render(){
+    return (
+      <>
+        <h4>Enter value</h4>
+        <form onSubmit={this.submitMessage}>
+          <input type = "text" value = {this.state.input} onChange = {this.handleChange}/><br/>
+          <button>Add</button>
+        </form>
+        <ul>
+          {this.props.messages.map((message, idx)=>{
+            return ( <li key = {idx}>{message}</li> )
+          })}
+        </ul>
+      </>
+    )
+  }
+}
+
+const mapStateToProps = (state) =>{
+  return {messages: state}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message))
+    }
+  }
+}
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational)
+
+class AppWrapper extends React.Component{
+  render(){
+    return (
+      <Provider store = {this.props.store}>
+        <Container />
+      </Provider>
+    )
+  }
+}
+
+export default AppWrapper;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+
+const ADD = 'ADD'
+
+const messageReducer = (state = [], action)=>{
+  switch(action.type){
+    case ADD:
+      return [...state, action.message];
+    default:
+      return state
+  }
+}
+
+const store = createStore(messageReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App store = {store}/>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
